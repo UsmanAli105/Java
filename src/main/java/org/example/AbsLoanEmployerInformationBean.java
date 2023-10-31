e53e6593a814c5c9460d62b3fef150a5733e5a48
SyedUsman.AliBukhari <usman40e@gmail.com>
2023-10-31 16:59:28 +0500

    Ok

--- src/main/java/org/example/AbsLoanEmployerInformationBean.java
+++ src/main/java/org/example/AbsLoanEmployerInformationBean.java
@@ -24,4 +24,84 @@ public class AbsLoanEmployerInformationBean {
     protected String phone1;
     @JsonProperty("PHONE2")
     protected String phone2;
+
+    public long getFileNo() {
+        return fileNo;
+    }
+
+    public void setFileNo(long fileNo) {
+        this.fileNo = fileNo;
+    }
+
+    public long getTranxNo() {
+        return tranxNo;
+    }
+
+    public void setTranxNo(long tranxNo) {
+        this.tranxNo = tranxNo;
+    }
+
+    public String getSeqNo() {
+        return seqNo;
+    }
+
+    public void setSeqNo(String seqNo) {
+        this.seqNo = seqNo;
+    }
+
+    public String getReportedOn() {
+        return reportedOn;
+    }
+
+    public void setReportedOn(String reportedOn) {
+        this.reportedOn = reportedOn;
+    }
+
+    public DataSection getEmployer() {
+        return employer;
+    }
+
+    public void setEmployer(DataSection employer) {
+        this.employer = employer;
+    }
+
+    public DataSection getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(DataSection designation) {
+        this.designation = designation;
+    }
+
+    public DataSection getSelfEmployed() {
+        return selfEmployed;
+    }
+
+    public void setSelfEmployed(DataSection selfEmployed) {
+        this.selfEmployed = selfEmployed;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhone1() {
+        return phone1;
+    }
+
+    public void setPhone1(String phone1) {
+        this.phone1 = phone1;
+    }
+
+    public String getPhone2() {
+        return phone2;
+    }
+
+    public void setPhone2(String phone2) {
+        this.phone2 = phone2;
+    }
 }
--- src/main/java/org/example/AbsLoanHomeInformationBean.java
+++ src/main/java/org/example/AbsLoanHomeInformationBean.java
@@ -24,4 +24,84 @@ public class AbsLoanHomeInformationBean {
     protected String phone1;
     @JsonProperty("PHONE2")
     protected String phone2;
+
+    public long getFileNo() {
+        return fileNo;
+    }
+
+    public void setFileNo(long fileNo) {
+        this.fileNo = fileNo;
+    }
+
+    public long getTranxNo() {
+        return tranxNo;
+    }
+
+    public void setTranxNo(long tranxNo) {
+        this.tranxNo = tranxNo;
+    }
+
+    public String getSeqNo() {
+        return seqNo;
+    }
+
+    public void setSeqNo(String seqNo) {
+        this.seqNo = seqNo;
+    }
+
+    public String getReportedOn() {
+        return reportedOn;
+    }
+
+    public void setReportedOn(String reportedOn) {
+        this.reportedOn = reportedOn;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPermanentAddress() {
+        return permanentAddress;
+    }
+
+    public void setPermanentAddress(String permanentAddress) {
+        this.permanentAddress = permanentAddress;
+    }
+
+    public String getPermanentCity() {
+        return permanentCity;
+    }
+
+    public void setPermanentCity(String permanentCity) {
+        this.permanentCity = permanentCity;
+    }
+
+    public String getPhone1() {
+        return phone1;
+    }
+
+    public void setPhone1(String phone1) {
+        this.phone1 = phone1;
+    }
+
+    public String getPhone2() {
+        return phone2;
+    }
+
+    public void setPhone2(String phone2) {
+        this.phone2 = phone2;
+    }
 }
